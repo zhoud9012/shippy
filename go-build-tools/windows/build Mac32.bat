b8c26d084c20cfdbe15574a0dd9688896ce8bac4
zhoud01 <zhoud01@mingyuanyun.com>
2019-01-07 16:31:37 +0800

    交叉编译脚本

---
+++ go-build-tools/windows/build Mac32.bat
@@ -0,0 +1,27 @@
+@echo off
+@color 06
+cls
+echo Press Ctrl+c at any time to quit.
+for /f "delims=" %%i in ("%cd%") do set folder=%%~ni
+set name=
+set /p name=name(%folder%):
+if defined name (
+    set name=%name%
+) else (
+    set name=%folder%
+)
+set defaultV=1.0.0
+set version=
+set /p version=version(%defaultV%):
+if defined version (
+    set version=%version%
+) else (
+    set version=%defaultV%
+)
+echo building...
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=386
+go build -o %name%.%version% main.go
+echo end...
+pause
